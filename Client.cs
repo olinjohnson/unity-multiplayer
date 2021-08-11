9945e0381884b033966da37329aea4ba6e0b2cba
olinjohnson <56653927+olinjohnson@users.noreply.github.com>
2021-08-10 21:02:11 -0400

    Updated Client.cs for use with unity

--- Client.cs
+++ Client.cs
@@ -1,33 +1,99 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
 using System;
-using System.Net;
 using System.Net.Sockets;
 using System.Text;
+using Newtonsoft.Json;
 
-class Client
+public class ClientController : MonoBehaviour
 {
-    static void Main(string[] args)
+    private int port = 7622;
+    private string host = "127.0.0.1";
+
+    private TcpClient client;
+    private NetworkStream ns;
+
+    private Rigidbody player;
+
+    public GameObject playerPrefab;
+
+    private string username;
+
+    // Start is called before the first frame update
+    void Start()
     {
-        int port = 7622;
-        string host = "127.0.0.1";
+        // Initialize client and network stream
+        client = new TcpClient(host, port);
+        ns = client.GetStream();
 
-        //Initialize empty byte array to use as buffer
-        byte[] buffer = new byte[1024];
+        // Store reference to the player
+        player = GameObject.Find("player").GetComponent<Rigidbody>();
 
-        //Connection class
-        TcpClient client = new TcpClient(host, port);
+        // Initial connection message containing name
+        username = "theactualnotabuildapp";
+        ns.Write(Encoding.ASCII.GetBytes(username), 0, username.Length);
+    }
 
-        //Class for data transfer over socket
-        NetworkStream ns = client.GetStream();
-
-        while (true)
+    void FixedUpdate()
+    {
+        // Create player instance of self to send to server
+        Player self = new Player()
         {
-            Console.WriteLine("Message: ");
-            string data = Console.ReadLine();
-            buffer = Encoding.ASCII.GetBytes(data);
-            ns.Write(buffer, 0, buffer.Length);
+            username = username,
+            position = new float[]
+            {
+                player.transform.position.x,
+                player.transform.position.y,
+                player.transform.position.z
+            }
+        };
+        // Send to server
+        string serializedPacket = JsonConvert.SerializeObject(self);
+        ns.Write(Encoding.ASCII.GetBytes(serializedPacket), 0, serializedPacket.Length);
 
-            ns.Read(buffer, 0, buffer.Length);
-            Console.WriteLine(Encoding.ASCII.GetString(buffer));
+        // Read response from server about other players and store players in a list
+        byte[] buffer = new byte[1024];
+        int bytesRead = ns.Read(buffer, 0, buffer.Length);
+        string parsedBuffer = Encoding.ASCII.GetString(buffer).Substring(0, bytesRead);
+        try
+        {
+            List<Player> deserializedBuffer = JsonConvert.DeserializeObject<List<Player>>(parsedBuffer);
+            UpdatePlayers(deserializedBuffer);
+        }
+        catch
+        {
+            Debug.Log("Couldn't update");
         }
     }
+
+    // Function to update the positions of other players
+    void UpdatePlayers(List<Player> p)
+    {
+        foreach (Player otherPlayer in p)
+        {
+            if (GameObject.Find(otherPlayer.username) == null)
+            {
+                GameObject newPlayer = Instantiate(playerPrefab, new Vector3(0, 5.65f, 0), Quaternion.identity);
+                newPlayer.name = otherPlayer.username;
+            }
+            else
+            {
+                Vector3 GOPosition = new Vector3(otherPlayer.position[0], otherPlayer.position[1], otherPlayer.position[2]);
+                GameObject.Find(otherPlayer.username).GetComponent<Rigidbody>().transform.position = GOPosition;
+            }
+        }
+    }
+
+    void OnApplicationQuit()
+    {
+        ns.Close();
+    }
+}
+
+// Player class
+class Player
+{
+    public string username;
+    public float[] position;
 }
